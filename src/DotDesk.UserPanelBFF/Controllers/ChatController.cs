327f2e22a4675fde235a4cc30cb59c690ca6025f
JpDotFranca <jpdotfranca@outlook.com>
2024-12-07 15:19:33 -0300

    fix:
    - Adding bff project for user panel;

---
+++ src/DotDesk.UserPanelBFF/Controllers/ChatController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DotDesk.UserPanelBFF.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ChatController : ControllerBase
+    {
+        public async Task<IActionResult> Get(int conversationId)
+        {
+            return await Task.FromResult(Ok());
+        }
+    }
+}
